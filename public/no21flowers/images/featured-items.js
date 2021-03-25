bda3f7241e1fa8d37e722dbb14b07da177d41efb
k.dubov <example@example.com>
2021-03-25 20:49:07 +0300

    box fix autowidth

--- public/no21flowers/images/featured-items.js
+++ public/no21flowers/images/featured-items.js
@@ -4,7 +4,7 @@ $(function () {
         carousels: [{
             selector: '.gallery-style .gallery-menu',
             options: {
-                // loop: true,
+                loop: true,
                 nav: false,
                 autoplay: false,
                 autoplayTimeout: 5000,
