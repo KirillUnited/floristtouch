a4a150ca0dd24bc7f3827e3c747e67ed13ab3c4c
k.dubov <example@example.com>
2021-03-25 22:23:36 +0300

    test

--- public/no21flowers/images/featured-items.js
+++ public/no21flowers/images/featured-items.js
@@ -19,32 +19,7 @@ $(function () {
                     $('body').css('overflow', 'hidden');
                 },
                 items: 9,
-                // onInitialized: fixOwl,
-                // onRefreshed: fixOwl
-                // slideBy: 4,
-                // responsive: {
-                //     0: {
-                //         items: 2,
-                //         margin: 20
-                //     },
-                //     480: {
-                //         items: 2,
-                //         margin: 25
-                //     },
-                //     768: {
-                //         items: 5,
-                //         margin: 30
-                //     },
-                //     991: {
-                //         items: 6
-                //     },
-                //     1200: {
-                //         items: 6
-                //     },
-                //     1920: {
-                //         items: 6
-                //     }
-                // }
+                onInitialized: fixOwl
             }
         }, {
             selector: '.gallery-style .gallery-carousel',
@@ -93,30 +68,21 @@ $(function () {
         }]
     }
     if (typeof $.fn.owlCarousel == 'function') {
-        
-        // $(model.carousels[0].selector).owlCarousel(model.carousels[0].options);
-        // $(model.carousels[0].selector).trigger('refresh.owl.carousel');
-        // $(model.carousels[1].selector).owlCarousel(model.carousels[1].options);
         for (const { selector, options } of model.carousels) {
-            // $(selector).owlCarousel(options).trigger('refresh.owl.carousel');
-            const promise = new Promise((resolve, reject) => {
+            new Promise((resolve, reject) => {
                 $(selector).owlCarousel(options);
                 resolve();
-            });
-
-            promise.then(() => {
-                setTimeout(() => {
-                    $(selector).trigger('refresh.owl.carousel');                    
-                }, 50);
+            }).then(() => {
+                fixOwl(selector);
             });
 
             // disable scroll
             $(selector).on('drag.owl.carousel', function (event) {
                 document.ontouchmove = function (e) {
                     e.preventDefault();
-                } 
+                }
             });
-    
+
             // enable scroll
             $(selector).on('dragged.owl.carousel', function (event) {
                 document.ontouchmove = function (e) {
@@ -126,24 +92,9 @@ $(function () {
         }
     }
 
-    function fixOwl(){
-        // var $stage = $('.owl-stage'),
-        //     stageW = $stage.width(),
-        //     $el = $('.owl-item'),
-        //     elW = 0;
-        // $el.each(function() {
-        //     elW += $(this).width()+ +($(this).css("margin-right").slice(0, -2))
-        // });
-        // if ( elW > stageW ) {
-        //     $stage.width( elW );
-        // };
-        // $('.gallery-style .gallery-menu').trigger( 'refresh.owl.carousel' );
-        var owlData = $(this.$element).data('owl.carousel');
-        console.log(this, owlData);
-        $('.gallery-style .gallery-menu').trigger('change.owl.carousel', {
-            mouseDrag: false,
-            touchDrag: false,
-            pullDrag: false
-          });
+    function fixOwl(selector) {
+        setTimeout(() => {
+            $(selector).trigger('refresh.owl.carousel');
+        }, 50);
     }
 });
